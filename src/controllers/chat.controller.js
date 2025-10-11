8968242ba89946eca4a4cb6c86b32a036b20f4fa
Udo Igwe Uchechukwu <udoigweuchechukwu@gmail.com>
2025-10-11 23:43:10 +0100

    chore: genai AI models integration

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,8 +1,7 @@
 const pool = require("../utils/dbConfig");
 const moment = require("moment");
-const OpenAI = require("openai");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
 const CustomError = require("../utils/CustomError");
+const { GoogleGenAI } = require("@google/genai");
 
 module.exports = {
     createChat: async (req, res, next) => {
@@ -11,17 +10,16 @@ module.exports = {
 
         let connection;
 
+        const ai = new GoogleGenAI({
+            apiKey: process.env.GOOGLE_GENAI_API_KEY,
+        });
+
         try {
             // Get a connection from the pool
             connection = await pool.getConnection();
 
             //start db transaction
             await connection.beginTransaction();
-
-            //instantiate GoogleAI client
-            const genAI = new GoogleGenerativeAI(
-                process.env.GOOGLE_AI_SECRET_KEY
-            );
 
             //save user message
             await connection.execute(
@@ -36,39 +34,29 @@ module.exports = {
                 [user_id, message]
             );
 
-            //send to GoogleAI
-            const response = await openai.chat.completions.create({
-                model: "deepseek-chat",
-                messages: [
-                    {
-                        role: "system",
-                        content: "You are a helpful assistant",
-                    },
-                    {
-                        role: "user",
-                        content: message,
-                    },
-                ],
-                stream: false, // Explicitly disable streaming
-                max_tokens: 1000,
-                temperature: 0.7,
+            const response = await ai.models.generateContent({
+                model: "gemini-2.0-flash",
+                contents: message,
+                generation_config: {
+                    max_output_tokens: 50, // roughly controls word count
+                },
             });
 
-            //get reply from chatgpt
-            const reply = response.choices[0].message.content;
+            //get reply from gemini
+            const reply = response.text;
 
-            //save response from chatgpt
+            //save response from gemini
             await connection.execute(
                 `
                     INSERT INTO knowledge_hub_chats
                     (
                         user_id,
-                        role,
+                        sender_role,
                         message
                     )
                     VALUES (?, 'assistant', ?)
                 `,
-                [user_id, message]
+                [user_id, reply]
             );
 
             //start db commit
@@ -84,6 +72,85 @@ module.exports = {
             next(e);
         } finally {
             connection ? connection.release() : null;
+        }
+    },
+    getChats: async (req, res, next) => {
+        const { user_id } = req.query;
+
+        const page = req.query.page ? parseInt(req.query.page) : null;
+        const perPage = req.query.perPage ? parseInt(req.query.perPage) : null;
+
+        let query = `
+            SELECT *
+            FROM knowledge_hub_chats 
+            WHERE 1 = 1`;
+        const queryParams = [];
+
+        let query2 = `
+            SELECT COUNT(*) AS total_records 
+            FROM knowledge_hub_chats 
+            WHERE 1 = 1
+        `;
+        const queryParams2 = [];
+
+        if (user_id) {
+            query += " AND user_id = ?";
+            queryParams.push(user_id);
+
+            query2 += " AND user_id = ?";
+            queryParams2.push(user_id);
+        }
+
+        query += " ORDER BY created_at ASC";
+
+        if (page && perPage) {
+            const offset = (page - 1) * perPage;
+            query += " LIMIT ?, ?";
+            queryParams.push(offset);
+            queryParams.push(perPage);
+        }
+
+        let connection;
+
+        try {
+            connection = await pool.getConnection();
+
+            const [data] = await connection.execute(query, queryParams);
+            const [total] = await connection.execute(query2, queryParams2);
+
+            /* PAGINATION DETAILS */
+
+            //total records
+            const totalRecords = parseInt(total[0].total_records);
+
+            // Calculate total pages if perPage is specified
+            const totalPages = perPage
+                ? Math.ceil(totalRecords / perPage)
+                : null;
+
+            // Calculate next and previous pages based on provided page and totalPages
+            const nextPage =
+                page && totalPages && page < totalPages ? page + 1 : null;
+            const prevPage = page && page > 1 ? page - 1 : null;
+
+            res.json({
+                error: false,
+                data,
+                paginationData: {
+                    totalRecords,
+                    totalPages,
+                    currentPage: page,
+                    itemsPerPage: perPage,
+                    nextPage,
+                    prevPage,
+                },
+            });
+        } catch (e) {
+            next(e);
+        } finally {
+            if (connection) {
+                connection.release();
+            }
         }
     },
 };
