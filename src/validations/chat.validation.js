2d74cf765074c5bdcad9f4326b7b1c3e79bcfcaf
Uchechukwu Udo <udoigweuchechukwu@gmail.com>
2025-11-25 12:50:39 +0100

    chore: AI chat validation expecting either a message or an image or both

--- src/validations/chat.validation.js
+++ src/validations/chat.validation.js
@@ -2,8 +2,16 @@ const { body } = require("express-validator");
 
 module.exports = {
     createChat: [
-        body("message")
-            .exists({ checkFalsy: true })
-            .withMessage("Message is required"),
+        body().custom((_, { req }) => {
+            const message = !req.body.message ? null : req.body.message;
+            const image = req?.files?.image;
+
+            // 🔥 Validate input
+            if ((!message || message.trim() === "") && !image) {
+                throw "Please provide at least a message or an image.";
+            }
+
+            return true;
+        }),
     ],
 };
